77b63331c2921763a68f0b523d72d55e18a9fe55
Hinoki <93910087+HinokiSu@users.noreply.github.com>
2023-04-18 17:12:07 +0800

    Delete GetPublicIP.vb

--- GetPublicIP.vb
+++
@@ -1,5 +0,0 @@
-Dim webClient As New System.Net.WebClient()
-Dim utf8 As New System.Text.UTF8Encoding()
-' Get public ip from URL by Https
-' @params ip String in/out
-ip = utf8.GetString(webClient.DownloadData("https://api.ipify.org"))
